d150771f7f37eceaa76475c1f0cb1471a7dc0e4b
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2023-10-16 01:44:08 +0530

    Fixed the issue of greeting user

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,8 @@ def main():
     print("Hello, world!")
     #printing hello world in spanish
     print("Hola Mundo")
+    #printing hello world in german
+    print("Bonjour, le monde !")
 
 if __name__ == "__main__":
     main()
