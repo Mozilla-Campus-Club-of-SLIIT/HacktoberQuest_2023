e53835baa9831e201baa129a690ef690d415710f
Sourav Suresh <sourav.s@visualyze.ai>
2023-10-16 11:38:37 +0530

    goodbye function

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,13 @@ def main():
     print("Hola Mundo")
     #printing hello world in german
     print("Bonjour, le monde !")
+def goodbye():
+    print("Goodbye")
+    #printing Goodbye in spanish
+    print("adiós")
+    #printing Goodbye in german
+    print("Verabschiedung")
 
 if __name__ == "__main__":
     main()
+    goodbye()
