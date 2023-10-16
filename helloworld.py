ada1a1b63ad5bc33bc24dbe82cc31c10d92488e7
Russell Peiris <ardpeiris@gmail.com>
2023-10-16 20:31:30 +0530

    added hello world in sinhala

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,8 @@ def main():
     print("Hola Mundo")
     #printing hello world in german
     print("Bonjour, le monde !")
+    #printing hello world in sinhalese
+    print("ආයුබෝවන් ලෝකය")
 
 if __name__ == "__main__":
     main()
