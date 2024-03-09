fbddac885fc0c7277db60061dd3eaed29ceba2ae
Adrian Torres <adriantorrescashcrate@gmail.com>
2024-03-09 10:12:59 -0700

    added Hawaiian translation

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,8 @@ def main():
     print("Hola Mundo")
     #printing hello world in german
     print("Bonjour, le monde !")
+    #printing hello world in Hawaiian 
+    print("Aloha, honua!")
 
 if __name__ == "__main__":
     main()
